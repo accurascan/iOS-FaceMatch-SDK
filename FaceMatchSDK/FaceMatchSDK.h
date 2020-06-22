6016ef17f90754b186b9db5e0c9414889825d03f
kuldeep <kuldeep@technozer.com>
2020-06-22 14:15:50 +0530

    init setup

---
+++ FaceMatchSDK/FaceMatchSDK.h
@@ -0,0 +1,26 @@
+//
+//  FaceMatchSDK.h
+//  FaceMatchSDK
+//
+//  Created by Technozer on 15/06/20.
+//  Copyright © 2020 Technozer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaceMatchSDK.
+FOUNDATION_EXPORT double FaceMatchSDKVersionNumber;
+
+//! Project version string for FaceMatchSDK.
+FOUNDATION_EXPORT const unsigned char FaceMatchSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaceMatchSDK/PublicHeader.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <MessageUI/MessageUI.h>
+#import "GlobalMethods.h"
+#import "EngineWrapper.h"
+#import "NSFaceRegion.h"
+#include "CustomAFNetWorking.h"
+#import "FaceView.h"
+#import "ImageHelper.h"
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FaceMatchSDK' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FaceMatchSDK
+
+ pod 'IQKeyboardManagerSwift'
+ pod 'SVProgressHUD'
+ pod 'Alamofire'
+ pod 'AFNetworking', '~> 3.0'
+ pod 'SDWebImage', '~>3.7'
+ pod 'OpenCV', '3.4.2'
+
+  target 'FaceMatchSDKTests' do
+    # Pods for testing
+  end
+
+end
